c40b67b963ddf5232f54d03d44dc6eed7d1f01ff
maxfqs <maxfqs@gmail.com>
2017-10-14 00:59:18 +0200

    Add offer panel open/close logic

--- .vscode/typing.lua
+++ .vscode/typing.lua
@@ -8,6 +8,7 @@
 
 -- CLASS DECLARATION
 --# assume global class CM
+--# assume global class CA_Model
 --# assume global class CA_UIC
 --# assume global class CA_Component
 --# assume global class CA_TimeTriggerContext
@@ -29,6 +30,14 @@
 --# assume CA_UIContext.string: string
 
 
+-- MODEL
+--# assume CA_Model.is_player_turn: method() --> boolean
+
+
+-- UIC
+--# assume CA_UIC.Visible: method() --> boolean
+
+
 -- CAMPAIGN MANAGER
 --# assume CM.add_listener: method(
 --#     handler: string,
@@ -38,11 +47,14 @@
 --#     shouldRepeat: boolean
 --# )
 --# assume CM.add_time_trigger: method(name: string, duration: number)
+--# assume CM.model: method() --> CA_Model
 --# assume CM.remove_listener: method(handler: string)
+--# assume CM.ui_root: method() --> CA_UIC
 
 
 -- GLOBAL FUNCTIONS
 --# assume global is_uicomponent: function(arg: any) --> boolean
+--# assume global find_uicomponent_by_table: function(uic: CA_UIC, path: vector<string>) --> CA_UIC
 --# assume global uicomponent_to_str: function(uic: CA_UIC) --> string
 
 
--- UI/DiplomacyDropdown.lua
+++ UI/DiplomacyDropdown.lua
@@ -5,20 +5,75 @@
 -- Handle the diplomatic panel
 -- ===========================================================================
 
+local Console = require("Core/Console");
+local Timer = require("Core/Timer");
 local Panel = require("UI/Panel");
+
 local DiplomacyDropdown = {};
+local UIC = {};
+
+
+function DiplomacyDropdown.init() 
+    DiplomacyDropdown.offerPanelOpened = false;
+    DiplomacyDropdown.stopLoop = false;
+
+    local root = cm:ui_root();
+    
+    UIC.offer = find_uicomponent_by_table(root, {"diplomacy_dropdown", "offers_panel"});
+end
+
+function DiplomacyDropdown.loop() 
+    if DiplomacyDropdown.stopLoop then return end
+    
+    -- Catch the offer panel opening on faction double click
+    if not DiplomacyDropdown.offerPanelOpened and UIC.offer:Visible() then
+        DiplomacyDropdown.offerPanelOpened = true;
+        Console.log("Open offer panel", "UI");
+    end
+
+    Timer.nextTick(DiplomacyDropdown.loop);
+end
 
 
 --v function(context: CA_UIContext)
 function DiplomacyDropdown.click(context) 
+    local name = context.string;
 
+    if not DiplomacyDropdown.offerPanelOpened then
+        -- offer panel is opening stop here
+        if name == "button_ok" then
+            DiplomacyDropdown.offerPanelOpened = true;
+            Console.log("Open offer panel", "UI");
+            return;
+        end
+
+    else -- Offer panel is opened
+        Timer.nextTick(function()
+            -- The offers panel can only be closed by pressing the button_cancel uic
+            -- but this id is not unique, it may come up when the player cancel an offer
+            -- we look at the offer panel visibility one tick after the event to determine
+            -- if we should close it or not
+            if name == "button_cancel" and not UIC.offer:Visible() then
+                DiplomacyDropdown.offerPanelOpened = false;
+                Console.log("Close offer panel", "UI");
+                return;
+            end
+        end)
+    end
 end
 
 function DiplomacyDropdown.open() 
+    -- abort if it's not the player that opened the panel
+    if not cm:model():is_player_turn() then return false end
+    
+    DiplomacyDropdown.init();
+    DiplomacyDropdown.loop();
+
     return true;
 end
 
 function DiplomacyDropdown.close()
+    DiplomacyDropdown.stopLoop = true;
     return true;
 end
 
